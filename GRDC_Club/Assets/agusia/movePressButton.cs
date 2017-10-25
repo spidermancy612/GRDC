f47d7b38cf7200acc579c7e140bd73817a8e6fd9
Unknown <redknives13@gmail.com>
2017-10-25 17:41:27 -0400

    Basic Movement UI
    got UI system for the movement implementation done

---
+++ GRDC_Club/Assets/agusia/movePressButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movePressButton : MonoBehaviour {
+	[Tooltip ("Object references for buttons")]
+	public GameObject[] inputField = new GameObject[6];
+
+
+	public void checkValues () {
+		
+		for (int i = 0; i < inputField.Length; i++) {
+			Debug.Log (i + ": "+ inputField[i].GetComponent<InputField>().text);
+		}
+	}
+}
