b761d3cf78ef8a7e01be2386124976a9ba0c5922
CameronJA\cameron <cameron.andress@hotmail.com>
2017-11-13 19:27:50 -0500

    stash

---
+++ GRDC_Club/Assets/astroidspawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class astroidspawner : MonoBehaviour {
+	public Gameobject astroid;
+
+	// Use this for initialization
+	void Start () {
+		Gameobject rock = Instantiate (astroid, transform.position, transform.rotation);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
