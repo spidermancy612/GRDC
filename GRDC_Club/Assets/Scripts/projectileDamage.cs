9d53872e4bef1c38359e4f54c5a8412914a16ef7
Liam <leeman1230@yahoo.ca>
2017-11-14 21:19:51 -0500

    Added health and damage scripts with documentation to the game files. Health script requires code for what to do when health reaches 0 as no action has been specified yet. Currently it will print to log that the object has "died". Decided not to kill object since that could disable main camera for game.

---
+++ GRDC_Club/Assets/Scripts/projectileDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]   // Make sure a RigidBody on this object for it to move
+
+public class projectileDamage : MonoBehaviour {
+
+    [Tooltip("Damage projectile applies")]
+    public float damage;                                // Damage the projectile does when it impacts an object with a shipHealth
+    [Tooltip("Tag on objects to apply damage to")]
+    public string theTag;                               // Tag to be looking for on object that enters the trigger
+
+    private Rigidbody rb;                               // Reference to Rigidbody component   
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called at the start of the scene
+    void Start () {
+        //Error logging
+		if (damage <= 0) { Debug.LogError("DEVELOPER ERROR - Bad Variable - Damage has not been properly set on " + gameObject.name); }
+        if (tag.Equals("")) { Debug.LogError("DEVELOPER ERROR  - Null Variable - Tag has not been set by on the " + gameObject.name + " object"); }
+
+        rb = GetComponent<Rigidbody>();                                                                             // Set reference for Rigidbody component
+	}
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called when a collider enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == theTag)                                                                         // Check if the collider entering the trigger has the tagwe're looking for
+        {
+            other.GetComponent<shipHealth>().damageTaken += damage;                                                     // Apply damage to object we hit
+            Destroy(this.gameObject);                                                                                   // Destroy this projectile
+        }
+    }
+}
---
+++ GRDC_Club/Assets/Scripts/shipHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipHealth : MonoBehaviour {
+
+    [Tooltip("Maximum health for the object")]
+    public float maxHealth;                     // Max Health the unit can have - set by developer
+    [HideInInspector]
+    public float damageTaken;                   // Damage to be taken at the end of each frame - set by another script
+
+    private float currentHealth;                // Current health
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called at start of scene
+	void Start () {
+        //Error logging
+        if (maxHealth <= 0) { Debug.LogError("DEVELOPER ERROR - Bad Variable - Max health has not been set properly on " + gameObject.name); }
+
+        currentHealth = maxHealth;                                                                              // Set current health to developer specified max health
+	}
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called every frame
+    private void Update()
+    {
+        if (currentHealth <= 0)                                                                                 // Check if the objects health has fallen to 0
+        {
+            Debug.Log(gameObject.name + " has died, but I don't know what to do with this information");            // The unit died, still need to integrate death systems
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called at the end of every frame
+    void LateUpdate () {
+        //Check if any other script has given us damage to take
+		if (damageTaken > 0)                                                                                    // Check if any damage has been given to this script
+        {
+            currentHealth = currentHealth - damageTaken;                                                            // Apply damage to current health
+            damageTaken = 0;                                                                                        // Reset damage taken to 0 for next frame
+        }
+	}
+}
