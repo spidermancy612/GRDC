f0955f6ef76833a88507c2dc1c7c15d4caa73ece
CameronJA\cameron <cameron.andress@hotmail.com>
2017-11-27 20:44:38 -0500

    Here are the physics/ code for the asteroid object.  It currently rotates in 3 dimensions at random speeds and directions and shoots off into space at a random direction (not the y - up down plane) and I am still in the process of figuring out the collision physics (trying to make it reflect perfectly in rotation and velocity after a collision).

---
+++ GRDC_Club/Assets/aster.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+
+
+public class aster : MonoBehaviour {
+	public GameObject asteroid;
+	private Rigidbody rb; 
+	int randLeft;
+	int randUp;
+	int randForward;
+	int randForceX;
+	int randForceZ;
+	double startMag;
+
+	// Use this for initialization
+	void Start () {
+		randLeft = Random.Range(-100,100);
+		randUp = Random.Range(-100,100);
+		randForward = Random.Range(-100,100);
+		randForceX = Random.Range(-200,200);
+		randForceZ = Random.Range(-200,200); 
+
+		rb = GetComponent <Rigidbody> ();
+		rb.useGravity = false;
+		rb.AddForce (new Vector3(randForceX, 0, randForceZ));
+		rb.AddTorque (Vector3.left * randLeft);
+		rb.AddTorque (Vector3.up * randUp);
+		rb.AddTorque (Vector3.forward * randForward);
+		startMag = Vector3.Magnitude;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// expand on this more (we are trying to set velocity in a reflected manner)
+		if (Vector3.Magnitude < startMag) {
+			rb.AddForce (rb.velocity.Normalize() * Vector3.Magnitude / (Vector3.Magnitude / startMag));
+		}
+	}
+			/*
+	private void OnCollisionEnter(Collision collision){
+		rb.AddTorque (Vector3.left * -randLeft);
+		rb.AddTorque (Vector3.up * -randUp);
+		rb.AddTorque (Vector3.forward * -randForward);
+	}*/
+};
