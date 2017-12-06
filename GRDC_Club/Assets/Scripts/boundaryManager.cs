1e4ee435fdb1617c484bee54ed7ab88213def61f
Liam <leeman1230@yahoo.ca>
2017-12-05 22:16:09 -0500

    Finished making the walls manager

---
+++ GRDC_Club/Assets/Scripts/boundaryManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundaryManager : MonoBehaviour {
+
+    public float playspaceSize;                 // Width and Height of the playspace
+    [Space(10)]
+    public string asteriodTag;                  // Tag for asteroids
+    public string shipTag;                      // Tag for ships
+    public string projectileTag;                // Tag for projectiles
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called when the object is created or activated
+    void Awake () {
+        setupRightWall();
+        setupLeftWall();
+        setupTopWall();
+        setupBottomWall();
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Create the right wall
+    private void setupRightWall ()
+    {
+        GameObject boundaryRight = GameObject.CreatePrimitive(PrimitiveType.Cube);                              // Create right boundary object
+        boundaryRight.transform.position = transform.right * playspaceSize / 2;                                 // Set the position of the object
+        boundaryRight.transform.localScale = new Vector3(1, 1, playspaceSize);                                  // Scale object to size
+        BoxCollider boxR = boundaryRight.AddComponent(typeof(BoxCollider)) as BoxCollider;                      // Add a box collider to object
+        boxR.isTrigger = true;                                                                                  // Set isTrigger to true
+        boundaryRight.AddComponent<playspaceWallManager>();                                                     // Add playspaceWallManager script
+        boundaryRight.GetComponent<playspaceWallManager>().updateTags(shipTag,asteriodTag,projectileTag);       // Send the tags to the wall
+        
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Create the left wall
+    private void setupLeftWall ()
+    {
+        GameObject boundaryLeft = GameObject.CreatePrimitive(PrimitiveType.Cube);                               // Create left boundary object
+        boundaryLeft.transform.position = transform.right * playspaceSize / 2 * -1;                             // Set the position of the object
+        boundaryLeft.transform.localScale = new Vector3(1, 1, playspaceSize);                                   // Set the scale of the object
+        BoxCollider boxL = boundaryLeft.AddComponent(typeof(BoxCollider)) as BoxCollider;                       // Add a box collider 
+        boxL.isTrigger = true;                                                                                  // Set the box collider to a trigger
+        boundaryLeft.AddComponent<playspaceWallManager>();                                                      // Add playspaceWallManager script
+        boundaryLeft.GetComponent<playspaceWallManager>().updateTags(shipTag, asteriodTag, projectileTag);      // Send the tags to the walls
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Create the top wall
+    private void setupTopWall ()
+    {
+        GameObject boundaryTop = GameObject.CreatePrimitive(PrimitiveType.Cube);                                // Creae the top boundary object
+        boundaryTop.transform.position = transform.forward * playspaceSize / 2;                                 // Set the position of the object
+        boundaryTop.transform.localScale = new Vector3(playspaceSize, 1, 1);                                    // Scale the object
+        BoxCollider boxT = boundaryTop.AddComponent(typeof(BoxCollider)) as BoxCollider;                        // Attach a box collider
+        boxT.isTrigger = true;                                                                                  // Set the box collider to a trigger
+        boundaryTop.AddComponent<playspaceWallManager>();                                                       // Attach playspaceWallManager to the wall
+        boundaryTop.GetComponent<playspaceWallManager>().updateTags(shipTag, asteriodTag, projectileTag);       // Update the tags on the wall
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Create the bottom wall
+    private void setupBottomWall ()
+    {
+        GameObject boundaryBottom = GameObject.CreatePrimitive(PrimitiveType.Cube);                             // Create the bottom boundary object
+        boundaryBottom.transform.position = transform.forward * playspaceSize / 2 * -1;                         // Set the position of the object
+        boundaryBottom.transform.localScale = new Vector3(playspaceSize, 1, 1);                                 // Scale the object
+        BoxCollider boxB = boundaryBottom.AddComponent(typeof(BoxCollider)) as BoxCollider;                     // Attach a box collider
+        boxB.isTrigger = true;                                                                                  // Set the collider to a trigger
+        boundaryBottom.AddComponent<playspaceWallManager>();                                                    // Attach a playspaceWallManager to the object
+        boundaryBottom.GetComponent<playspaceWallManager>().updateTags(shipTag, asteriodTag, projectileTag);    // Update the tags on the wall
+    }
+}
---
+++ GRDC_Club/Assets/Scripts/playspaceWallManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playspaceWallManager : MonoBehaviour {
+
+    /*
+     * Created by Liam Baillie
+     * December 5, 2017
+     * 
+     * Script is attached to the playspace walls by the boundaryManager script found on the GAMEMANAGER.
+     * This script handles how to interact with any object in the scene when it enters the trigger for 
+     * the wall. 
+     */
+
+    [HideInInspector]
+    public string shipTag;                  // Tag to check for ship object - informed by boundaryManager
+    [HideInInspector]
+    public string asteriodTag;              // Tag to check for asteriod objects - informed by boundaryManager
+    [HideInInspector]
+    public string projectileTag;            // Tag to check for projectile object - informed by boundaryManager
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+    //Method runs on start of scene
+	void Start () {
+		
+	}
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called when a collider enters the trigger volume
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check for ship 
+        if (other.gameObject.tag == shipTag)
+        {
+            shipAction(other.gameObject);
+        } 
+        //Check for asteriod
+        else if (other.gameObject.tag == asteriodTag)
+        {
+            asteriodAction(other.gameObject);
+        }
+        //Check for projectile
+        else if (other.gameObject.name == projectileTag)
+        {
+            projectileAction(other.gameObject);
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //How to affect the ship object when it enters the trigger
+    private void shipAction (GameObject ship)
+    {
+        float xPos = ship.transform.position.x * -1;
+        float yPos = ship.transform.position.y * -1;
+        float zPos = ship.transform.position.z * -1;
+
+        ship.transform.position = new Vector3(xPos, yPos, zPos);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //How to affect the asteriod object when it enters the volume
+    private void asteriodAction (GameObject asteriod)
+    {
+        float xPos = asteriod.transform.position.x * -1;
+        float yPos = asteriod.transform.position.y * -1;
+        float zPos = asteriod.transform.position.z * -1;
+
+        asteriod.transform.position = new Vector3(xPos, yPos, zPos);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //How to affect the projectile when it enters the volume
+    private void projectileAction (GameObject projectile)
+    {
+        Destroy(projectile);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    public void updateTags (string s, string a, string p)
+    {
+        shipTag = s;
+        asteriodTag = a;
+        projectileTag = p;
+    }
+}
