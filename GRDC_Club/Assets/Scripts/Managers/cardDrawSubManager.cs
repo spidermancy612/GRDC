813d7b131a92c2667f02187e8c10d64bf2389626
Liam Baillie <Leeman1230@gmail.com>
2018-02-06 19:35:44 -0500

    Working on new script system

---
+++ GRDC_Club/Assets/Scripts/Managers/cardDrawSubManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardDrawSubManager : MonoBehaviour {
+
+    /*
+     * Script acts as a SubManager for the CardsManager.
+     * Handles storing and drawing cards for all players.
+     * 
+     * Deals with when all cards are used and draws decks when needed
+     */
+
+    private int playerCount;
+
+    private Dictionary<int, string[]> playerDictionary;
+
+
+#region Unity Default Methods
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called once at start of scene
+    void Start () {
+		
+	}
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Method called every frame
+    void Update () {
+		
+	}
+    #endregion
+
+
+    #region Private Methods
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    private bool checkForEmptyDeck (int playerIndex)
+    {
+        return true;
+    }
+    #endregion
+
+
+    #region Public Methods
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    public void runInitialLoad (int pCount)
+    {
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    public int[] getPlayerDeck (int playerNum)
+    {
+        return null;
+    }
+    #endregion
+}
---
+++ GRDC_Club/Assets/Scripts/Managers/referenceManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class referenceManager : MonoBehaviour {
+
+    /*
+     * Class holds object references for the all scripts in the scene
+     */
+
+    [Header("Canvases")]
+    public GameObject showDeckCanvas;
+    public GameObject applyThrustCanvas;
+    public GameObject applyWeaponsCanvas;
+
+    //[Space(5)]
+    //[Header("Scripts")]
+    
+}
